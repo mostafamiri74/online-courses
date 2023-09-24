88967aeb74dd300b1dff7f170ac77d9247c9ee3d
mostafa miri <mostafamiri12345@gmail.com>
2023-09-24 15:17:50 +0330

    Add routing to cart and course component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { PanelComponent } from './modules/panel/panel.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { CourseComponent } from './modules/course/course.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,16 @@ const routes: Routes = [
     component: PanelComponent,
     loadChildren: () =>
       import('./modules/panel/panel.module').then((m) => m.PanelModule),
+  },
+  {
+    path: 'course/:course-name',
+    loadChildren: () =>
+      import('./modules/course/course.module').then((m) => m.CourseModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./modules/cart/cart.module').then((m) => m.CartModule),
   },
 ];
 

4fe007a6c2eb45277935896495eb495c9da68377
mostafa miri <mostafamiri12345@gmail.com>
2023-09-24 15:18:16 +0330

    Add cart signal

--- src/app/modules/cart/cart-routing.module.ts
+++ src/app/modules/cart/cart-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: CartComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CartRoutingModule { }
+export class CartRoutingModule {}
--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/core/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
+  cartItems: any[] = [];
+  constructor(public cartService: CartService) {}
 
+  ngOnInit() {}
+
+  remove(i: number) {
+    this.cartService.removeCourseSignal(i);
+  }
 }
